131be65f79d83a80aa4aabcecec1fb3a28127308
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-24 17:46:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var img = document.getElementById('mov');
 var marginLeft = 0;
 function  moveRight() {
-   marginLeft +=10;
+   marginLeft +=1;
    img.style.marginLeft = marginLeft +   'px';
 }
 img.onclick =function() {
