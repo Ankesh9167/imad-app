bdf744d97082829d3cde601960a47d3732199f43
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-24 17:56:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,11 @@ var htmlTemplates =
 `;
 return htmlTemplates;
 }
-
+var counter=0;
+app.get('/counter',function(req,res){
+    counter +=1;
+    res.send(counter.toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
