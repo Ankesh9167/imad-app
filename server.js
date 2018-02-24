99c858871d2c0d4a17f5408ac4fc366e4ef73ff6
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-24 16:34:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var  articles = {
             </p>`
 },
    ' article-two': {
-   title : "Article-two !Ankesh!",
+     title : "Article-two !Ankesh!",
     heading : "Article two;",
     date : "25 feb 2018",
     content:  `<p>
