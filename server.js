91881a3abe90025f98f408b99b8ad35e6ca19f18
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-24 16:41:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,16 +48,8 @@ var  articles = {
                 There were no apparent weaknesses in Tendulkar's game. He could score all around the wicket, off both front foot and back, could tune his technique to suit every condition, temper his game to suit every situation, and made runs in all parts of the world in all conditions
             </p>`
         
-    },
-    'article-four': {
-    title : "Article-four !Ankesh!",
-    heading : "Article four;",
-    date : "27 feb 2018",
-    content:  `<p>
-                 Sachin Tendulkar has been the most complete batsman of his time, the most prolific runmaker of all time, and arguably the biggest cricket icon the game has ever known. His batting was based on the purest principles: perfect balance, economy of movement, precision in stroke-making, and that intangible quality given only to geniuses - anticipation. If he didn't have a signature stroke - the upright, back-foot punch comes close - it's because he was equally proficient at each of the full range of orthodox shots (and plenty of improvised ones as well) and can pull them out at will.
-                </p>`
-        
     };
+   
 function createTemplates(data) {
     var title = data.title;
     var date = data.date;
