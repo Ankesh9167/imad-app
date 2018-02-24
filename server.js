48c50c13ac0ce0a97089eaccde0510a3f743fbc6
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-24 14:11:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
- res.sendFile('Article one wiil be served');   
+ res.send('Article one wiil be served');   
 });
 app.get('/article-two',function(req,res){
- res.sendFile('Article two wiil be served');   
+ res.send('Article two wiil be served');   
 });
 app.get('/article-three',function(req,res){
- res.sendFile('Article three wiil be served');   
+ res.send('Article three wiil be served');   
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
