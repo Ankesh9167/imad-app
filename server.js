0b8c7eacb6e262aa4b5298973125c1de09d2e014
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-24 15:33:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,62 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var  articleone  = {
+    title : "Article-one !Ankesh!",
+    heading : "Article one",
+    date : "24 feb 2018",
+    content:  `<p>
+                 Sachin Tendulkar has been the most complete batsman of his time, the most prolific runmaker of all time, and arguably the biggest cricket icon the game has ever known. His batting was based on the purest principles: perfect balance, economy of movement, precision in stroke-making, and that intangible quality given only to geniuses - anticipation. If he didn't have a signature stroke - the upright, back-foot punch comes close - it's because he was equally proficient at each of the full range of orthodox shots (and plenty of improvised ones as well) and can pull them out at will.
+                
+            </p>
+            <p>
+                There were no apparent weaknesses in Tendulkar's game. He could score all around the wicket, off both front foot and back, could tune his technique to suit every condition, temper his game to suit every situation, and made runs in all parts of the world in all conditions
+            </p>
+            <p>
+                Some of his finest performances came against Australia, the overwhelmingly dominant team of his era. His century as a 19-year-old on a lightning-fast pitch at the WACA is considered one of the best innings ever to have been played in Australia. A few years later he received the ultimate compliment from the ultimate batsman: Don Bradman confided to his wife that Tendulkar reminded him of himself.
+            </p>`
+};
+function createTemplates(data) {
+    var title = data.title;
+    var date = data.date;
+    var content = data.content;
+    var heading = data.heading;
+var template = 
+    `<html>
+    <head>
+        <title>
+           $[title]
+        </title>
+           <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "container">
+        <div>
+            <a href = '/'>Home</a>
+        </div>
+        <hr/>
+        <h3>
+            $[heading]
+        </h3>
+        <div>
+            $[date]
+        </div>
+        <div>
+         $[content]  
+            
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplates;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+  res.send(creatTemplates(article-one)); 
 });
 app.get('/article-two',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
