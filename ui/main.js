7656092749a999d65fef9164d091fe941cbfe9ab
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-25 23:31:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,8 +17,8 @@ button.onclick = function () {
   
   var nameInput = document.getElementById('name');
   var name = nameInput.value;
-  var send = document.getElementById('send_btn');
-  send.onclick = function() {
+  var submit = document.getElementById('send_btn');
+  submit.onclick = function() {
  var names = ["name1","name2","name3","name4"] ;
  var list = '';
  var i;
