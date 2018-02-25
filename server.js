fc5c6187491ccfe97d4f5e96ae34430c5e9e4a3d
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-25 18:13:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,29 +20,7 @@ var articleone = {
                 Some of his finest performances came against Australia, the overwhelmingly dominant team of his era. His century as a 19-year-old on a lightning-fast pitch at the WACA is considered one of the best innings ever to have been played in Australia. A few years later he received the ultimate compliment from the ultimate batsman: Don Bradman confided to his wife that Tendulkar reminded him of himself.
             </p>`
 };
-//'article-two': {
-    title : "Article-2 !Ankesh!",
-    heading : "Article 2;",
-    date : "28 feb 2018",
-    content:  `<p>
-                 Sachin Tendulkar has been the most complete batsman of his time, the most prolific runmaker of all time, and arguably the biggest cricket icon the game has ever known. His batting was based on the purest principles: perfect balance, economy of movement, precision in stroke-making, and that intangible quality given only to geniuses - anticipation. If he didn't have a signature stroke - the upright, back-foot punch comes close - it's because he was equally proficient at each of the full range of orthodox shots (and plenty of improvised ones as well) and can pull them out at will.
-           
-            </p>`
-},
-//'article-three':{
-         title : "Article-three !Ankesh!",
-    heading : "Article three",
-    date : "26 feb 2018",
-    content:  `<p>
-                 Sachin Tendulkar has been the most complete batsman of his time, the most prolific runmaker of all time, and arguably the biggest cricket icon the game has ever known. His batting was based on the purest principles: perfect balance, economy of movement, precision in stroke-making, and that intangible quality given only to geniuses - anticipation. If he didn't have a signature stroke - the upright, back-foot punch comes close - it's because he was equally proficient at each of the full range of orthodox shots (and plenty of improvised ones as well) and can pull them out at will.Ankesh
-                
-            </p>
-            <p>
-                There were no apparent weaknesses in Tendulkar's game. He could score all around the wicket, off both front foot and back, could tune his technique to suit every condition, temper his game to suit every situation, and made runs in all parts of the world in all conditions
-            </p>`
-        
-    }
-//};
+
 
 function createTemplates(data) {
     var title = data.title;
