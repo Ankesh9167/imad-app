3f74344519ac3c45be24badfe341b7fb410cafd6
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-25 16:41:38 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,16 @@
+var  button = document.getElementById('counter');
+  button.onclick = function () {
+      var request =  new XMLHttpRequest ();
+      request.onreadystatechange = function() {
+          if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('counter');
+            span.innerHTML = counter.toString();
+            
+        }
+        }
+          };
+        requst.open('GET','http://ankeshphapale1234.imad.hasura-app.io/counter',true);
+          request.send(null);
+  }; 
