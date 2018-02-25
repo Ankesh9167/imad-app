b21cee02767b78e742770ef53fd881795c1a5254
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-25 18:00:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-   res.sendFile(createTemplates(article-one));
+   res.send(createTemplates(article-one));
 });
 /*app.get('/:articleName',function(req, res){
     var articleName = req.params.articleName;
