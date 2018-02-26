b4bdc2306b4b77e00f63b47db5b3d512bafd1037
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-26 23:15:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,4 +63,41 @@ button.onclick = function () {
  
   
   };
+  var submit1 = document.getElementById('submit1_btn');
+  submit1.onclick = function() {
+      
+   //creat a request object
+      var request =  new XMLHttpRequest ();
+      
+      //capture a requst and save it to variable
+      request.onreadystatechange = function() {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              //take some action
+        if (request.status === 200) {
+           var comments = request.responseText
+           comment == JSON.parse(comments);
+        var list = '';
+         var i;
+        for(i =0;i<names.length;i++)
+           {
+       list +='<li>'+comments[i]+'</li>';
+     
+            }
+        var ul = document.getElementById('namelist');
+        ul.innerHTML =list; 
+            
+        }
+        }
+        
+          };
+          
+          // make request
+          var nameInput = document.getElementById('comment');
+  var comment= nameInput.value;
+        request.open('GET','http://ankeshphapale1234.imad.hasura-app.io/submit-name?name=' +commment ,true);
+          request.send(null);   
+ 
+  
+  };
+  
   
