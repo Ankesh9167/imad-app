a89f66b268974dfbcc81645997e2b53ae5a73fbf
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-02-26 22:50:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,15 @@
+//counter code
 var  button = document.getElementById('counter');
+
 button.onclick = function () {
+    
+    //creat a request object
       var request =  new XMLHttpRequest ();
+      
+      //capture a requst and save it to variable
       request.onreadystatechange = function() {
           if (request.readyState === XMLHttpRequest.DONE) {
+              //take some action
         if (request.status === 200) {
             var counter = request.responseText;
             var span = document.getElementById('counter');
@@ -10,24 +17,49 @@ button.onclick = function () {
             
         }
         }
+        
           };
+          
+          // make request
         request.open('GET','http://ankeshphapale1234.imad.hasura-app.io/counter',true);
           request.send(null);
   };
   
+  //submit name
   var nameInput = document.getElementById('name');
   var name = nameInput.value;
   var submit = document.getElementById('submit_btn');
   submit.onclick = function() {
- var names = ["name1","name2","name3","name4"] ;
- var list = '';
- var i;
- for(i =0;i<names.length;i++)
- {
-     list +=' <li>' + names[i] + '</li>';
+      
+   //creat a request object
+      var request =  new XMLHttpRequest ();
+      
+      //capture a requst and save it to variable
+      request.onreadystatechange = function() {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              //take some action
+        if (request.status === 200) {
+           var names = request.responseText
+           names == JSON.parse(names);
+        var list = '';
+         var i;
+        for(i =0;i<names.length;i++)
+           {
+       list +=' <li>' + names[i] + '</li>';
      
- }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML =list;
+            }
+        var ul = document.getElementById('namelist');
+        ul.innerHTML =list; 
+            
+        }
+        }
+        
+          };
+          
+          // make request
+        request.open('GET','http://ankeshphapale1234.imad.hasura-app.io/submit-name?name='+name ,true);
+          request.send(null);   
+ 
   
   };
+  
