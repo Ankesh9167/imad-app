dd9ce186395969ed2e90dc4ef9107d4d254f1b58
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-03-05 10:16:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
-/*var config = {
+var Pool = require('pg').Pool;
+var config = {
     user : 'ankeshphapale1234',
     database: 'ankeshphapale1234',
     host:'db.imad.hasura-app.io',
     port: '5432',
     password:process.env.DB_PASSWORD
-};*/
+};
 var app = express();
 app.use(morgan('combined'));
 
