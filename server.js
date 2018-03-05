05f94dfa12be4af6ec44eb7201f439343a4e2809
Ankesh9167 <36410740+Ankesh9167@users.noreply.github.com>
2018-03-05 10:17:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ return htmlTemplates;
 }
 var Pool = new Pool(config);
 app.get('/test-db',function(req,res){
-  pool.query('SELECT * FROM test',function(err,result){
+  Pool.query('SELECT * FROM test',function(err,result){
       if(err){
           res.status(500).send(err.toString());
       }
